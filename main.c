2de4cd53d26275a64dfdcaa54fce277e0deded7e
damien venant-valéry <damien.venant@outlook.com>
2023-05-30 12:27:44 +0200

    Je ne sais pas quoi mettre

--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include "utils/convert.h"
+
+static void create_processus(const int);
 
 int main(int argc, char **argv) {
-  printf("Hello World\n");
+  int nb_processus;
+  if (argc != 2) {
+    printf("Il faut renseigner le nombre de processus esclave");
+    exit(1);
+  }
+
+  if ((nb_processus = stoi(argv[1])) == -1) {
+    printf("L'entrée n'est pas un nombre\n");
+    exit(1);
+  }
+
   return EXIT_SUCCESS;
+}
+
+
+static void create_processus(const int nb_processus) {
+  
 }
