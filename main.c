50b50a35779b74160c43e2cef5e029daca2a7204
damien venant-valéry <damien.venant@outlook.com>
2023-06-05 14:42:37 +0200

    Ajout de l'implémentation dans le main

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 #include "utils/convert.h"
 #include "utils/shared_mem.h"
+#include "utils/wrap_sem.h"
 
 void create_processus(const int);
 
@@ -38,8 +39,22 @@ void create_processus(const int nb_processus) {
 #define OFFSET_TAB_CHILDS 1
   int index;
   pid_t pid_observer;
+  id_sem id_mutex_proctect_sharedmemory;
   MemoirePartagee sharedmemory;
+
   sharedmemory = superMalloc(nb_processus + OFFSET_TAB_CHILDS);
+
+  if (sharedmemory.descriptor == -1) {
+    printf("Erreur lors de la création de la mémoire partagée\n");
+    exit(1);
+  }
+
+  id_mutex_proctect_sharedmemory = create_mutex();
+
+  if (id_mutex_proctect_sharedmemory == -1) {
+    printf("Erreur lors de la création du mutex");
+    exit(1);
+  }
 
   if (sharedmemory.descriptor == -1) {
     printf("superMalloc(): Erreur d'allocations");
@@ -49,8 +64,9 @@ void create_processus(const int nb_processus) {
     printf("PID Child esclave observer: %d\n", getpid());
     exit(0);
   }
-
+  mutex_lock(id_mutex_proctect_sharedmemory);
   addElement(&sharedmemory, pid_observer);
+  mutex_unlock(id_mutex_proctect_sharedmemory);
 
   for (index = 0; index < nb_processus; index++) {
     pid_t currentPid = fork();
@@ -58,8 +74,9 @@ void create_processus(const int nb_processus) {
       printf("PID Child esclave : %d\n", getpid());
       exit(0);
     }
-
+    mutex_unlock(id_mutex_proctect_sharedmemory);
     addElement(&sharedmemory, currentPid);
+    mutex_unlock(id_mutex_proctect_sharedmemory);
   }
 
   superFree(&sharedmemory);
