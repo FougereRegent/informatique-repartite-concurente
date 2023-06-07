569317aaf425a1594526b998c3da0ab3ce63bf80
damien venant-valéry <damien.venant@outlook.com>
2023-06-07 11:55:11 +0200

    Ajout des fonctions d'initialisation de l'observer

--- observateur/observateur.c
+++ observateur/observateur.c
@@ -19,19 +19,32 @@ typedef struct {
   int socket;
 } Annuary;
 
+/*Définition des variables globales*/
+static Annuary annuary;
+
 /*Définition des prototypes*/
 static void kill_process(const int code);
 static void loop(Annuary annuary);
 
 extern void initObservateur(MemoirePartagee *m) {
-  Annuary annuary = {.pids = m->adresse + OFFSET_OBSERVER_TAB,
-                     .size_tab = m->adresse + OFFSET_SIZE,
-                     .index_tab = m->adresse + OFFSET_INDEX};
+  id_sem id_mutex_shared_memory = create_mutex();
+  mutex_lock(id_mutex_shared_memory);
+  annuary.pids = m->adresse + OFFSET_OBSERVER_TAB;
+  annuary.size_tab = m->adresse + OFFSET_SIZE;
+  annuary.index_tab = m->adresse + OFFSET_INDEX;
+  mutex_unlock(id_mutex_shared_memory);
 
   loop(annuary);
-}
+};
 
-static void kill_procrss(const int code) { pid_t my_own_pid = getpid(); }
+static void kill_procrss(const int code) {
+  pid_t my_own_pid = getpid();
+  int index;
+  int size = *annuary.size_tab;
+
+  for (index = OFFSET_OBSERVER_TAB; index < size; index++) {
+  }
+}
 static void loop(Annuary annuary) {
   while (1) {
   }
