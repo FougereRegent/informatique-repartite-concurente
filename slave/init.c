8ff627c5fd4f57dd48b59fa060b5ed6900c1ce73
FouegereRegent <damien.venant@outlook.com>
2023-06-18 12:27:16 +0200

    Création des threads

--- slave/init.c
+++ slave/init.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,21 +6,42 @@
 #include <sys/signal.h>
 #include <unistd.h>
 
+#include "../utils/comunication.h"
 #include "../utils/wrap_signal.h"
 #include "init.h"
 
 #define BUFFER_SIZE 10
+#define NB_THREAD 4
+#define ERROR -1
+#define NO_ERROR 0
 
+static int init_buffer();
+static int init_threads();
 static void kill_process();
-static void thread_intelligence();
-static void thread_trace();
-static void thread_client();
-static void thread_server();
+static void *thread_intelligence();
+static void *thread_trace();
+static void *thread_client();
+static void *thread_server();
+
+/*Déclaration des variables static*/
+static pthread_t threads[NB_THREAD];
+static Message *buffer_trace_to_smart;
+static Message *buffer_smart_to_trace;
+static Message *buffer_smart_to_client;
+static Message *buffer_server_to_smart;
 
 extern void slave_init(PipeCommunication *pipe) {
   change_signal(SIGTERM, &kill_process);
-
+  /*Init buffer*/
+  if (init_buffer() == ERROR) {
+    perror("calloc : ");
+    exit(EXIT_FAILURE);
+  }
   /*Création des 4 threads*/
+  if (init_threads() == ERROR) {
+    perror("pthread() : ");
+    exit(EXIT_FAILURE);
+  }
 }
 
 static void kill_process() {
@@ -27,7 +49,29 @@ static void kill_process() {
   exit(0);
 }
 
-static void thread_intelligence() {}
-static void thread_trace() {}
-static void thread_client() {}
-static void thread_server() {}
+static int init_buffer() {
+  buffer_trace_to_smart = (Message *)calloc(BUFFER_SIZE, sizeof(Message));
+  buffer_smart_to_trace = (Message *)calloc(BUFFER_SIZE, sizeof(Message));
+  buffer_smart_to_client = (Message *)calloc(BUFFER_SIZE, sizeof(Message));
+  buffer_server_to_smart = (Message *)calloc(BUFFER_SIZE, sizeof(Message));
+
+  return buffer_smart_to_trace == NULL || buffer_smart_to_client == NULL ||
+                 buffer_trace_to_smart == NULL || buffer_server_to_smart
+             ? NO_ERROR
+             : ERROR;
+}
+
+static int init_threads() {
+  int result = 0;
+  result |= pthread_create(&threads[0], NULL, thread_trace, NULL);
+  result |= pthread_create(&threads[1], NULL, thread_intelligence, NULL);
+  result |= pthread_create(&threads[2], NULL, thread_server, NULL);
+  result |= pthread_create(&threads[3], NULL, thread_client, NULL);
+
+  return result != 0 ? ERROR : NO_ERROR;
+}
+
+static void *thread_intelligence() { return NULL; }
+static void *thread_trace() { return NULL; }
+static void *thread_client() { return NULL; }
+static void *thread_server() { return NULL; }
