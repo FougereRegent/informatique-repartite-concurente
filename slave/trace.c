cef490436be3fcb2d1a0538930c9ccb21114a1ef
FouegereRegent <damien.venant@outlook.com>
2023-06-19 23:52:39 +0200

    Je ne sais pas quoi mettre

--- slave/trace.c
+++ slave/trace.c
@@ -5,4 +5,11 @@
 
 extern void trace_loop(proc_cons_locker *locker_producteur,
                        proc_cons_locker *locker_consomateur,
-                       PipeCommunication *pipes) {}
+                       PipeCommunication *pipes) {
+
+  Message message_consomateur;
+
+  while (1) {
+    message_consomateur = consume(locker_consomateur);
+  }
+}
