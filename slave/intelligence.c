8a366d64844b45a90355ed6f01a0682db019915b
FouegereRegent <damien.venant@outlook.com>
2023-06-19 23:11:27 +0200

    Ajout de la partie intelligence

--- slave/intelligence.c
+++ slave/intelligence.c
@@ -3,13 +3,14 @@
 
 #include "intelligence.h"
 
-extern void smart_loop(proc_cons_locker *locker_producteur,
-                       proc_cons_locker *locker_consomateur) {
-  Message result;
+extern void smart_loop(proc_cons_locker *locker_producteur_client,
+                       proc_cons_locker *locker_consomateur_server,
+                       proc_cons_locker *locker_trace) {
+  Message result_server;
+  Message result_trace;
 
   while (1) {
-    result = consume(locker_consomateur);
-
-    printf("consume : %d %s\n", result.type, result.message);
+    result_server = consume(locker_consomateur_server);
+    product(locker_trace, result_server);
   }
 }
--- slave/intelligence.h
+++ slave/intelligence.h
@@ -3,6 +3,7 @@
 
 #include "../utils/productor_consomateur.h"
 
-extern void smart_loop(proc_cons_locker *locker_producteur,
-                       proc_cons_locker *locker_consomateur);
+extern void smart_loop(proc_cons_locker *locker_producteur_client,
+                       proc_cons_locker *locker_consomateur_server,
+                       proc_cons_locker *locker_trace);
 #endif
